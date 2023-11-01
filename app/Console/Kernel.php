caf68e34348f9f0a2c310b6c5435f3bb5d6fbd1f
David <75451291+dulkoss@users.noreply.github.com>
2023-11-01 20:30:45 +0100

    use new Console\Kernel#load to dynamically load commands

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,23 +2,14 @@
 
 namespace App\Console;
 
-use App\Console\Commands\ServeCommand;
 use Tavurn\Console\Kernel as BaseKernel;
 
 class Kernel extends BaseKernel
 {
-    protected array $commands = [
-        ServeCommand::class,
-    ];
-
     protected function commands(): void
     {
-        foreach ($this->commands as $command) {
-            if (is_string($command)) {
-                $command = $this->app->make($command);
-            }
-
-            $this->symfony->add($command);
-        }
+        $this->load(
+            base_path('app/Console/Commands'),
+        );
     }
 }
