523e585eef1d39d46c8ddd7760b6bfb337647cab
David <75451291+dulkoss@users.noreply.github.com>
2023-11-03 23:20:05 +0100

    basic templating example

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Tavurn\Support\Facades\Route;
+use Tavurn\Contracts\Http\Request;
 
-Route::get('/', function () {
-    return 'Welcome!';
-});
+Route::get('/[{name}]',
+    static fn (Request $request) => template('welcome.php', [
+        'name' => $request->get('name'),
+    ])
+);
